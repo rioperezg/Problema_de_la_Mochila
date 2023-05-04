051b703e20b7003c51948566b6c27cf8e3fb6378
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-05-04 10:01:51 +0200

    haciendo dataset

--- Dataset.py
+++ Dataset.py
@@ -3,7 +3,13 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
-%matplotlib inline
 
 
-Ds_Pokemon = 
+Ds_Pokemon = pd.read_csv("Datos/dataset.csv")
+Ds_Pokedex = pd.read_csv("Datos/pokedex.csv")
+columnas_drop= ["TIPO_1",
+                "TIPO_2",
+                "PUNTOS_VELOCIDAD",
+                "NOMBRE_GENERATIONS",
+                "LEGENDARIO"]
+Ds_Pokedex2 = Ds_Pokedex.drop(columns=columnas_drop)
